42787a75cc9b46ca77da87d4d02471f0101a339b
Jennifer Lindgren <jenli414@student.liu.se>
2016-09-14 15:06:25 +0200

    2016-09-14

--- wordchain/src/wordchain.cpp
+++ wordchain/src/wordchain.cpp
@@ -1,46 +1,48 @@
+/* Lab 2: Wordchain
+ * jenli414
+ * sabse455 */
+
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <queue>
 #include <stack>
-#include <unordered_set>
+#include <set>
 #include <sstream>
 
 
 using namespace std;
 
-
+// Function declarations.
 void printWelcomeMessage();
-void getWordsFromUser(string& w1, string& w2);
-void findShortestChain(string& w1, string& w2, stack<string>& result);
-void setDictionary(unordered_set<string>& dictionary);
-void setNeighbours(queue<string>& neighbours, string& currentWord, unordered_set<string>& usedWords, unordered_set<string>& dictionary);
-void addNeighbour(string& word, queue<string>& neighbours, unordered_set<string>& usedWords, unordered_set<string>& dictionary);
-bool isValidWord(string& word, unordered_set<string>& dictionary);
-void printResult(string& w1, string& w2, stack<string>& result);
+void getWordsFromUser(string& word1, string& word2);
+void findShortestChain(const string& word1, const string& word2, stack<string>& result);
+void setDictionary(set<string>& dictionary);
+void setNeighbours(queue<string>& neighbours, const string& currentWord, set<string>& usedWords, const set<string>& dictionary);
+void addNeighbour(const string& word, queue<string>& neighbours, set<string>& usedWords, const set<string>& dictionary);
+bool isValidWord(const string& word, const set<string>& dictionary);
+void printResult(const string& word1, const string& word2, stack<string>& result);
 void printEndMessage();
 
-
+// Constants.
 const string ALPHABET  = "abcdefghijklmnopqrstuvwxyz";
 
-
+/* Main part of the program. Prints a welcome message, gets two words from the user
+ * and finds the shortest path between them by moving between valid english words that
+ * only differ by one letter. */
 int main() {
     printWelcomeMessage();
-    string w1;
-    string w2;
-    getWordsFromUser(w1, w2);
+    string word1;
+    string word2;
+    getWordsFromUser(word1, word2);
     stack<string> result;
-    findShortestChain(w1, w2, result);
-    if (!result.empty()) {
-        printResult(w1, w2, result);
-    } else {
-        cout << "Couldn't find path from " << w2 << " back to " << w1 << "." << endl;
-    }
+    findShortestChain(word1, word2, result);
+    printResult(word1, word2, result);
     printEndMessage();
     return 0;
 }
 
-
+// Prints welcome message.
 void printWelcomeMessage() {
     cout << "Welcome to TDDD86 Word Chain." << endl;
     cout << "If you give me two English words, I will transform the" << endl;
@@ -48,50 +50,50 @@ void printWelcomeMessage() {
     cout << endl;
 }
 
-
-void getWordsFromUser(string& w1, string& w2){
+// Gets two words from the user and saves them in word1 and word2.
+void getWordsFromUser(string& word1, string& word2){
     cout << "Please type two words: ";
-    cin >> w1 >> w2;
+    cin >> word1 >> word2;
     cout << endl;
 }
 
-
-void findShortestChain(string& w1, string& w2, stack<string>& result) {
-    queue<stack<string>> q;
-    stack<string> firstStack;
-    firstStack.push(w1);
-    stack<string> currentStack;
+// Finds the shortest path between word1 and word2 and stores the result in result.
+void findShortestChain(const string& word1, const string& word2, stack<string>& result) {
+    queue<stack<string>> chains;
+    stack<string> firstChain;
+    firstChain.push(word1);
+    stack<string> currentChain;
     string currentWord;
-    unordered_set<string> dictionary;
+    set<string> dictionary;
     setDictionary(dictionary);
-    unordered_set<string> usedWords = {w1};
+    set<string> usedWords = {word1};
     queue<string> neighbours;
-    if (w1.length() == w2.length()) {
-        q.push(firstStack);
+    if (word1.length() == word2.length()) {
+        chains.push(firstChain);
     }
-    while (!q.empty()) {
-        currentStack = q.front();
-        q.pop();
-        currentWord = currentStack.top();
-        if (currentWord == w2) {
-            result = currentStack;
-            while (!q.empty()) {
-                q.pop();
+    while (!chains.empty()) {
+        currentChain = chains.front();
+        chains.pop();
+        currentWord = currentChain.top();
+        if (currentWord == word2) {
+            result = currentChain;
+            while (!chains.empty()) {
+                chains.pop();
             }
         } else {
             setNeighbours(neighbours, currentWord, usedWords, dictionary);
             while (!neighbours.empty()) {
-                stack<string> nextStack = currentStack;
-                nextStack.push(neighbours.front());
+                stack<string> nextChain = currentChain;
+                nextChain.push(neighbours.front());
                 neighbours.pop();
-                q.push(nextStack);
+                chains.push(nextChain);
             }
         }
     }
 }
 
-
-void setDictionary(unordered_set<string>& dictionary) {
+// Opens dictionary.txt and stores the words from it in a set<string>.
+void setDictionary(set<string>& dictionary) {
     ifstream input;
     input.open("dictionary.txt");
     string word;
@@ -101,15 +103,17 @@ void setDictionary(unordered_set<string>& dictionary) {
     input.close();
 }
 
-
-void setNeighbours(queue<string>& neighbours, string& currentWord, unordered_set<string>& usedWords, unordered_set<string>& dictionary) {
+// Sets the queue of valid neighbours of a word.
+void setNeighbours(queue<string>& neighbours, const string& currentWord,
+                   set<string>& usedWords, const set<string>& dictionary) {
     string word;
     int currentWordLength = currentWord.length();
     int alphabethLength = ALPHABET.length();
     for (int j = 0; j < alphabethLength; ++j) {
         for (int i = 0; i < currentWordLength - 1; ++i) {
             stringstream ss;
-            ss << currentWord.substr(0,i) << ALPHABET[j] << currentWord.substr(i + 1,currentWord.length() - i);
+            ss << currentWord.substr(0,i) << ALPHABET[j] <<
+                  currentWord.substr(i + 1,currentWord.length() - i);
             word = ss.str();
             addNeighbour(word, neighbours, usedWords, dictionary);
         }
@@ -123,26 +127,26 @@ void setNeighbours(queue<string>& neighbours, string& currentWord, unordered_set
     }
 }
 
-
-void addNeighbour(string& word, queue<string>& neighbours, unordered_set<string>& usedWords, unordered_set<string>& dictionary) {
+// Adds a word to neighbours if it hasn't already been used and is a valid word.
+void addNeighbour(const string& word, queue<string>& neighbours, set<string>& usedWords,
+                  const set<string>& dictionary) {
     if ((usedWords.count(word) != 1) && (isValidWord(word, dictionary))) {
         neighbours.push(word);
         usedWords.insert(word);
     }
 }
 
-
-bool isValidWord(string& word, unordered_set<string>& dictionary) {
-    if (dictionary.count(word) == 1) {
-        return true;
-    } else {
-        return false;
-    }
+// Returns whether a word is valid by checking if it's in the dictionary.
+bool isValidWord(const string& word, const set<string>& dictionary) {
+    return dictionary.count(word) == 1;
 }
 
-
-void printResult(string& w1, string& w2, stack<string>& result) {
-    cout << "Chain from " << w2 << " back to " << w1 << ":" << endl;
+// Prints the result to the user.
+void printResult(const string& word1, const string& word2, stack<string>& result) {
+    cout << "Chain from " << word2 << " back to " << word1 << ":" << endl;
+    if (result.empty()) {
+        cout << "Couldn't find path from " << word2 << " back to " << word1 << ".";
+    }
     while (!result.empty()) {
         cout << result.top() << " ";
         result.pop();
@@ -150,7 +154,7 @@ void printResult(string& w1, string& w2, stack<string>& result) {
     cout << endl;
 }
 
-
+// Prints end message to user.
 void printEndMessage() {
     cout << "Have a nice day!" << endl;
 }
